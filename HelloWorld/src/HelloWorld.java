89599e90c2ffa082d73fabc69765f39558a69f8b
GMPark <pgm050612@gmail.com>
2023-04-03 11:39:43 +0900

    a1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,9 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Hello his world");
-		System.out.println("Hello your world");
-		System.out.println("Hello her world");
+		System.out.println("Hello his a world");
 	}
 
 }
